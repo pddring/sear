2f4d0666eba76b3cebe0591d88fbcc55afa6431e
Pete Dring <dringp@fulford.york.sch.uk>
2025-10-17 10:54:23 +0100

    print output devices

--- src/audio.py
+++ src/audio.py
@@ -3,4 +3,6 @@ import pyaudio
 if __name__ == "__main__":
   print("Testing audio")
   audio = pyaudio.PyAudio()
+  for i in range(audio.get_device_count()):
+    print(audio.get_device_info_by_index(i))
   audio.terminate()
