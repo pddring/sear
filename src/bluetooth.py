5b1fdaaec83daa6887692498721134d245fb35ad
Pete Dring <dringp@fulford.york.sch.uk>
2026-02-28 18:35:24 +0000

    Working status button

--- src/bluetooth.py
+++ src/bluetooth.py
@@ -1,10 +1,11 @@
 from threading import Thread
 import os
 import pexpect
+import re
 
 class Bluetooth:
     def __init__(self):
-        pass
+        self.devices = {}
 
     def set_power(self, on):
         if on:
@@ -12,28 +13,60 @@ class Bluetooth:
             p = pexpect.spawn("bluetoothctl")
             p.sendline("power on")
             p.expect("Changing power on succeeded", timeout=2)
-            print(p.after)
             p.close()
         else:
             print("Turning bluetooth off")
             p = pexpect.spawn("bluetoothctl")
             p.sendline("power off")
             p.expect("Changing power off succeeded", timeout=2)
-            print(p.after)
             p.close()
-
-    def get_devices(self):
-        p = pexpect.spawn("bluetoothctl")
-        p.sendline("devices")
-        while True:
-            p.expect("Device ([0-9A-F:]+) (.*?)\r", timeout=1)
-            print(p.after)
+        
+    def update_status(self, id):
+        p = pexpect.spawn("bluetoothctl", encoding="utf-8")
+        p.sendline(f"info {id}")
+        connected = p.expect(["Connected: yes", "Connected: no"]) == 0
+        battery = 0
+        if connected:
+            p.expect("Battery Percentage: .* \\((\\d+)\\)")
+            battery = p.match.group(1)
         p.close()
 
+        self.devices[id]["connected"] = connected
+        self.devices[id]["battery"] = battery
+        return self.devices[id]
 
+    def get_devices(self):
+        p = pexpect.spawn("bluetoothctl", encoding="utf-8")
+        p.sendline("devices")
+        self.devices = {}
+        while True:
+            try:
+                p.expect("Device ([0-9A-F:]+) (.*?)\r\n", timeout=0.1)
+                device = p.after
+                m = re.match("Device ([0-9A-F:]+) (.*?)\r\n", device)
+                self.devices[m.group(1)] = {"name": m.group(2), "connected": False}
+                
+            except pexpect.TIMEOUT:
+                break
+        
+        for id in self.devices:
+            self.update_status(id)
+            
+            
+        p.close()
+        return self.devices
 
-    def connect(self):
-        print(f"Connecting to bluetooth device {self.device_id} (NOT IMPLEMENTED YET)")
+    def connect(self, id):
+        print(f"Connecting to bluetooth device {id}")
+        p = pexpect.spawn("bluetoothctl", encoding="utf-8")
+        p.sendline(f"connect {id}")
+        p.expect(f"Attempting to connect to {id}")
+        success = p.expect(["Connection successful", "not available"])==0
+        if id in self.devices:
+            self.devices[id]["connected"] = success
+        p.close()
+        return success
+
 
     def play_audio(self, filename="last_speech.wav", blocking=True):       
         if blocking:
@@ -53,11 +86,17 @@ if __name__ == "__main__":
     print("Testing bluetooth")
     b = Bluetooth()
     b.set_power(True)
-    b.get_devices()
+    print(b.get_devices())
+
     input("Wait")
-    b.set_power(False)
+    b.connect("11:75:58:EB:5F:CD")
+    input("Wait")
 
     while True:
         filename = input("wav file:")
+        if filename == "":
+            break
         b.play_audio(filename, False)
-    #
+    
+    b.set_power(False)
+
--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import camera
 import json
 import os
+import datetime
 #import audio
 import bluetooth
 import ai
@@ -15,13 +16,22 @@ class Sear(settings.Settings):
         self.buttons = buttons.ButtonManager()
         self.buttons.add_listener('MIDDLE', 'SHORT_PRESS', self.go_button)
 
+        self.buttons.add_listener('TOP', 'SHORT_PRESS', self.status_button)
+
         print(f"Creating AI client with API KEY: {self.settings['API_KEY']}")
         #self.ai = ai.AzureAI(self.settings["AZURE_KEY"], self.settings["AZURE_ENDPOINT"])
         self.ai = ai.GoogleAI(self.settings["API_KEY"])
 
         print("Initialising Bluetooth audio")
         self.audio = bluetooth.Bluetooth()
-        #self.audio.select_output_device_by_name("bluealsa")
+        self.audio.set_power(True)
+        devices = self.audio.get_devices()
+        
+        # connect to first previously connected device
+        for id in devices:
+            self.audio.connect(id)
+            self.settings["bluetooth_device"] = id
+            break
 
     def go_button(self, name):
         print("Taking picture")
@@ -43,6 +53,19 @@ class Sear(settings.Settings):
             s.ai.get_speech(response)
             print("Playing speech")
             s.audio.play_audio()"""
+        
+    def status_button(self, name):
+        d = datetime.datetime.now()
+        status = f"It's {d.strftime('%A %d %B at %I:%M %p')}. "
+        id = self.settings["bluetooth_device"]
+        b = self.audio.update_status(id)
+        if b["connected"]:
+            status += f"Connected to {b['name']} with battery at {b['battery']} percent"
+        else:
+            status += f"Not connected to bluetooth speakers or headphones"
+        s.ai.get_speech(self.settings["voice_prompt"] + "\n" + status, "last_status.wav")
+        s.audio.play_audio("last_status.wav")
+        
     
     def wait_for_buttons(self):
         print("Starting button server")
